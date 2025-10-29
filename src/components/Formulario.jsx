f595db5b9afbb1d406736ab68cef649476c61ea9
Valentina Iramain <valentinairamain2020@gmail.com>
2025-10-29 09:37:48 -0300

    se crea un array donde se guardara la lista de tareas, y un input donde recibira la tarea cargada

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,25 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ListaTareas from './ListaTareas';
+import { useState } from 'react';
 
 const Formulario = () => {
+    const [arrayTareas,setArrayTareas] = useState([])
+    const [inputTarea,setInputTarea] = useState('')
+
+    const handleSubmit = (e)=>{
+        console.log(inputTarea)
+        e.preventDefault()
+        setArrayTareas([...arrayTareas,inputTarea])
+        setInputTarea('')
+    }
+
     return (
         <>
-            <Form className='container'>
+            <Form className='container' onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="inputTarea">
                     <div className="d-flex flex-column flex-md-row gap-2 align-items-md-start align-items-center">
-                        <Form.Control type="text" placeholder="Ingrese la tarea" />
+                        <Form.Control type="text" placeholder="Ingrese la tarea" onChange={(e)=>setInputTarea(e.target.value)} value={inputTarea} />
                         <Button
                             variant="secondary"
                             type="submit"
